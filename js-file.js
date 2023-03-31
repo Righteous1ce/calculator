f6014dc7a0747593ab764ee7cab92344eb91d96c
Righteous1ice <hoplabradley@gmail.com>
2023-03-31 22:17:19 +0100

    Added the operate function and the variables for numbers and operators

--- js-file.js
+++ js-file.js
@@ -1,3 +1,7 @@
+var num1 = 0;
+var num2 = 0;
+var operator = ['+', '-', '/', '*'];
+
 const add = function(num1, num2) {
     return num1 + num2;
   }
@@ -14,4 +18,26 @@ const add = function(num1, num2) {
     return num1 / num2;
   }
   
+
+
+  function operate(num1, operator, num2) {
+    let result = 0;
+    switch (operator) {
+      case '+':
+      result = add(num1, num2);
+      break;
+      case '-':
+        result = subtract(num1, num2);
+        break;
+        case '*':
+          result = multiply(num1, num2);
+          break;
+          case '/':
+            result = divide(num1, num2);
+            break;
+            default:
+              console.log("Invalid Operator");
+    }
+    return result;
+  }
   
